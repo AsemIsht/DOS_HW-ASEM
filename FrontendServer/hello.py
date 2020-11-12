558b7da9598e9524cf7619e29296824bf962d2a8
Asem3 <isht.asem@stu.najah.edu>
2020-11-13 01:12:17 +0200

    first commit

---
+++ FrontendServer/hello.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+import requests
+import json
+
+app = Flask(__name__)
+
+url = 'http://192.168.121.134:5000'
+
+@app.route('/')
+def hello_world():
+    response = json.loads(requests.get(url).content)
+    print (response["age"])
+    return (str(response["age"]+1))
+
+@app.route('/search/<topic>')
+def search(topic):
+    return (topic)
+
+@app.route('/lookup/<item_number>')
+def lookup(item_number):
+    return (item_number)
+    
+@app.route('/buy/<item_number>')
+def buy(item_number):
+    newPrice = {'newPrice' : 50}
+    x = requests.post(url+'/update/1', data = newPrice)
+    response = json.loads(x.content)
+    print(str(response["ass"]))
+    
+    return (response)
+
+
+# Notes: 
+# return jsonify(a)
+# json.loads(requests.get(url).content) # --> response["age"]
+#
