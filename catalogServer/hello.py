f638b1571a1ce9f3635f275d15ffb61c93696e9a
Asem <isht.asem@stu.najah.edu>
2020-11-13 01:25:36 +0200

    catalog server upload initial commit

---
+++ catalogServer/hello.py
@@ -0,0 +1,36 @@
+from flask import Flask,jsonify,request
+import requests
+import json
+import sqlite3
+
+app = Flask(__name__)
+
+def ass():
+    return ("ss")
+
+@app.route('/')
+def hello_world():
+    db = sqlite3.connect('catalogDB.db')
+    cursor = db.cursor()
+
+    return "jsonify(a)"
+
+@app.route('/query/<item_number>')
+def query(item_number):
+    var = None
+    try:
+        var = int(item_number) + 0
+    except TypeError:
+        var = str(item_number)
+    
+    print(type(var) is int)
+    return (str(var))
+
+@app.route('/update/<item_number>', methods=['GET', 'POST']) #post
+def update(item_number):
+    x = ''
+    if request.method == 'POST':
+        x = request.form['newPrice']
+        print (x)
+        
+    return (jsonify({'ass':item_number + x}))
