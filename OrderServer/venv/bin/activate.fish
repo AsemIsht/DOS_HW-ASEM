974697f5b23966a43be79dc5cb2455b2d037685a
OrderServer <isht.asem@stu.najah.edu>
2020-11-13 02:07:02 +0200

    Order server: maintanance on enviroment

--- OrderServer/venv/bin/activate.fish
+++ OrderServer/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/orderserver/Desktop/my_flask_app/venv"
+set -gx VIRTUAL_ENV "/home/orderserver/Desktop/DOS_HW-ASEM/OrderServer/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
